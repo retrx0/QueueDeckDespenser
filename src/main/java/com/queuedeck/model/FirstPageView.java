e1576ac4c1c6353bbe12d49f3415be6d84cca2ab
retrx0 <abdulrahmaniliyasu86@gmail.com>
2020-07-08 20:05:18 +0200

    adding models1

---
+++ src/main/java/com/queuedeck/model/FirstPageView.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.queuedeck.model;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author ABDULRAHMAN ILLO
+ */
+public class FirstPageView extends AnchorPane{
+    
+    String name;
+    ImageView logo;
+    
+    
+}
---
+++ src/main/java/com/queuedeck/model/PageView.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.queuedeck.model;
+
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author ABDULRAHMAN ILLO
+ */
+public class PageView extends AnchorPane{
+    
+}
